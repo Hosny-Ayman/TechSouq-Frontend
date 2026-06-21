82d12c2b7ad49c46fd0c3e21ebc3f43bc93a7aea
Hosny-Ayman <hosnyaymandev@gmail.com>
2026-06-21 17:19:52 +0300

    fix: downgrade social login to 2.4.0 and update lock file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, PLATFORM_ID, inject } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -10,11 +10,11 @@ import {
 } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { MessageService } from 'primeng/api';
-import { isPlatformBrowser } from '@angular/common';
 
 import {
-  SocialAuthServiceConfig,
   GoogleLoginProvider,
+  SocialAuthServiceConfig,
+  SocialLoginModule,
 } from '@abacritt/angularx-social-login';
 
 export const appConfig: ApplicationConfig = {
@@ -25,34 +25,24 @@ export const appConfig: ApplicationConfig = {
     MessageService,
     provideHttpClient(withFetch(), withInterceptors([authInterceptor])),
 
+    importProvidersFrom(SocialLoginModule),
+
     {
       provide: 'SocialAuthServiceConfig',
-      useFactory: () => {
-        const platformId = inject(PLATFORM_ID);
-        const isBrowser = isPlatformBrowser(platformId);
-
-        if (isBrowser) {
-          return {
-            autoLogin: false,
-            providers: [
-              {
-                id: GoogleLoginProvider.PROVIDER_ID,
-                provider: new GoogleLoginProvider(
-                  '686495575320-pbl37bfoma2rmpu8v5j4d9nvlgb33sop.apps.googleusercontent.com',
-                ),
-              },
-            ],
-            onError: (err: any) => {
-              console.error('Google Auth Error:', err);
-            },
-          } as SocialAuthServiceConfig;
-        }
-
-        return {
-          autoLogin: false,
-          providers: [],
-        } as SocialAuthServiceConfig;
-      },
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '686495575320-pbl37bfoma2rmpu8v5j4d9nvlgb33sop.apps.googleusercontent.com',
+            ),
+          },
+        ],
+        onError: (err: any) => {
+          console.error('Google Auth Error:', err);
+        },
+      } as SocialAuthServiceConfig,
     },
   ],
 };
